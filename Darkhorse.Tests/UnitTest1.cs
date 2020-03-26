dcc6f92ba6b3134c0ff44b6824d2876ad49d14aa
uncheckederror <thomas.ryan@outlook.com>
2020-03-25 18:44:39 -0700

    Added a unit test and some example tests.

---
+++ Darkhorse.Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+using Darkhorse.DataAccess;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Darkhorse.Tests
+{
+    public class ConfigurationTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public ConfigurationTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void IsTheConfigurationFileValid()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("fcb4eb5f-a4e9-49af-9bb0-72b3a44ebda8")
+                .Build();
+
+            foreach (var con in config.AsEnumerable())
+            {
+                output.WriteLine(con.ToString());
+            }
+        }
+    }
+
+    public class LISPIntegrationTests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly IConfiguration configuration;
+        private readonly string connectionString;
+
+        public LISPIntegrationTests(ITestOutputHelper output)
+        {
+            this.output = output;
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("fcb4eb5f-a4e9-49af-9bb0-72b3a44ebda8")
+                .Build();
+            configuration = config;
+            connectionString = configuration.GetConnectionString("LISP");
+        }
+
+        [Fact]
+        public async Task RealPropertyAccountGetAsync()
+        {
+            int rpAcctId = 2361145;
+            var accounts = await RealPropertyAccount.GetAsync(rpAcctId, configuration.GetConnectionString("LISP"));
+            foreach (var account in accounts)
+            {
+                Assert.NotNull(account);
+                Assert.Equal(rpAcctId, account.RP_ACCT_ID);
+                output.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(account));
+            }
+        }
+    }
+}
