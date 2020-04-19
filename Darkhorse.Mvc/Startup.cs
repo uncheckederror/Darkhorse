c869318b4d50d58fcbf7a15253a1912df5de31f2
uncheckederror <thomas.ryan@outlook.com>
2020-04-19 13:54:47 -0700

    Minor changes.

--- Darkhorse.Mvc/Startup.cs
+++ Darkhorse.Mvc/Startup.cs
@@ -24,7 +24,7 @@ namespace DarkHorse.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddAuthentication(IISDefaults.AuthenticationScheme);
         }
 
