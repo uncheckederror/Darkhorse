828f97f5f5e1662b50bc7a33c6bb4e62ad9e303a
Erik Anderson <belleauwood@users.noreply.github.com>
2020-04-28 18:43:11 -0700

    Changes to BaseTableClass, added class extension to IDbConnection, and added/modified LIS tables related to plats.
    * Changed MODIFIED_DT in BaseTableClass to nullable DateTime.
    * Added class extension to create connection.
    * Added PLAT_BLOCKS, PLAT_ORIG_ACCTS, PLAT_SECTIONS.
    * PLATS: changed RECORDED_DT and VOLUME to nullable types.
    * REMARKS: changed PP_ACCT_ID, APPLICATION_ID, RP_ACCT_ID, END_DT, and HOLD_CODE_ID.
    * REMARKS: Added GetPlatsFormAsync() method.

---
+++ DarkHorse.DataAccess/PLAT_BLOCKS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DarkHorse.DataAccess
+{
+    public class PLAT_BLOCKS : BaseTableClass
+    {
+        #region Fields
+
+        public int PLAT_BLOCK_ID { get; set; }
+        public int PLAT_ID { get; set; }
+        public string BLOCK_NO { get; set; }
+        public int NO_LOTS { get; set; }
+
+        #endregion
+    }
+}
